7f429c7a3b2d8146965f5cff135f8c578cfb0738
Dan <danielarobinson2@gmail.com>
2021-12-15 11:10:12 +0000

    #comment HP-16 added put,get,post etc

--- Project/JS/index.js
+++ Project/JS/index.js
@@ -65,3 +65,129 @@ const renderFighter = (fighter, outputDiv) => {
   fighterCard.appendChild(newFighter);
   outputDiv.appendChild(fighterColumn);
 };
+
+document
+  .querySelector("#createGameForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const form = event.target;
+
+    const data = {
+      name: form.name.value,
+      divison: form.divison.value,
+      country: form.country.value,
+    };
+    console.log("DATA:", data);
+
+    axios
+      .post(`${baseURL}/createFighter`, data)
+      .then((res) => {
+        console.log(res);
+        getAllFighters();
+
+        form.reset();
+        form.name.focus();
+
+        alert(`${form.name.value} has been added :)`);
+      })
+      .catch((err) => console.log(err));
+  });
+
+const deleteFighter = (id) => {
+  axios
+    .delete(`${baseURL}/deleteFighter/${id}`)
+    .then((res) => {
+      console.log(res);
+      getAllFighters();
+
+      alert("Fighter successfully deleted");
+    })
+    .catch((err) => console.log(err));
+};
+
+const updateFighter = (id) => {
+  const data = {
+    name: document.getElementById("fighterName").value,
+    division: document.getElementById("fighterDivision").value,
+    country: document.getElementById("fighterCountry").value,
+  };
+  axios
+    .put(`${baseURL}/updateFighter/${id}`, data)
+    .then((res) => {
+      getAllFighters();
+
+      alert(
+        `${document.getElementById("figherName").value} has now been updated!`
+      );
+      createFighterForm.reset();
+    })
+    .catch((err) => console.log(err));
+};
+
+const getFighterById = () => {
+  axios
+    .get(`${baseURL}/getFighterById/${fighterId.value}`)
+    .then((res) => {
+      const fighter = res.data;
+
+      getByIdOutput.innerHTML = "";
+
+      renderFighter(fighter, getByIdOutput);
+    })
+    .catch((err) => console.log(err));
+};
+
+const getFighterByName = () => {
+  axios
+    .get(`${baseURL}/getFighterByName/${findFighterByName.value}`)
+    .then((res) => {
+      console.log(res);
+      const fighters = res.data;
+      console.log(fighters);
+      getFighterByName.innerHTML = "";
+
+      fighters.forEach((fighter) => renderFighter(fighter, getByNameOutput));
+    })
+    .catch((err) => console.log(err));
+};
+
+const getFighterByDivison = () => {
+  axios
+    .get(`${baseURL}/getFighterByDivison/${findFighterByDivison.value}`)
+    .then((res) => {
+      console.log(res);
+      const fighters = res.data;
+      console.log(fighters);
+      getFighterByDivison.innerHTML = "";
+
+      fighters.forEach((fighter) => renderFighter(fighter, getByNameOutput));
+    })
+    .catch((err) => console.log(err));
+};
+
+const getFighterByCountry = () => {
+  axios
+    .get(`${baseURL}/getFighterByCountry/${findFighterByCountry.value}`)
+    .then((res) => {
+      console.log(res);
+      const fighters = res.data;
+      console.log(fighters);
+      getFighterByCountry.innerHTML = "";
+
+      fighters.forEach((fighter) => renderFighter(fighter, getByNameOutput));
+    })
+    .catch((err) => console.log(err));
+};
+
+document
+  .querySelector("div#getFighterByName > button")
+  .addEventListener("click", getFighterByName);
+document
+  .querySelector("div#getFighterByDivision > button")
+  .addEventListener("click", getFighterByDivison);
+document
+  .querySelector("div#getFighterByCountry > button")
+  .addEventListener("click", getFighterByCountry);
+
+getAllFighters();
